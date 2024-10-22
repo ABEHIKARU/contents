899a6e1623ff9114d8d9e7097c3b78d167708baa
ABE <shinsotsukenshu@nipssc.cp.jp>
2024-10-22 15:37:37 +0900

    php

---
+++ sendform.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ささえタイ送信フォーム</title>
+    <link rel="stylesheet" href="css/sendform.css"> <!-- 外部CSSファイルの読み込み -->
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <!-- ロゴ -->
+            <div class="logo-container">
+                <a href="index.html">
+                    <img src="img/logo.png" alt="ささえタイのロゴ" class="logo">
+                </a>
+            </div>
+
+            <!-- メニュー -->
+            <nav class="menu-container">
+                <ul class="menu">
+                    <li><a href="index.html#sasaetaitoha">ささえタイとは</a></li>
+                    <li><a href="index.html#kinouichiran">機能一覧</a></li>
+                    <li><a href="index.html#kigyouzyouhou">企業情報</a></li>
+                </ul>
+            </nav>
+
+            <!-- 資料請求・お問い合わせボタン -->
+            <a href="sendform.php" target="_blank" class="cta-button">資料請求・お問い合わせ</a>
+        </div>
+    </header>
+
+    <h2>資料請求・お問い合わせフォーム</h2>
+    
+    <table>
+        <tr>
+            <td>氏名(漢字)&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="text" id="name_sei" name="name_sei" placeholder="姓" maxlength="20">
+                <input type="text" id="name_mei" name="name_mei" placeholder="名" maxlength="20">
+            </td>
+        </tr>
+        
+        <tr>
+            <td>氏名(フリガナ)&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="text" id="kana_name_sei" name="kana_name_sei" placeholder="セイ" maxlength="20">
+                <input type="text" id="kana_name_mei" name="kana_name_mei" placeholder="メイ" maxlength="20">
+            </td>
+        </tr>
+
+        <tr>
+            <td>会社名&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="text" id="com_name" name="com_name" maxlength="100">
+            </td>
+        </tr>
+
+        <tr>
+            <td>電話番号&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="tel" id="tel_num" name="tel_num" minlength="3" maxlength="4" size="4">-
+                <input type="tel" id="tel_num" name="tel_num" minlength="3" maxlength="4" size="4">-
+                <input type="tel" id="tel_num" name="tel_num" minlength="3" maxlength="4" size="4">
+            </td>
+        </tr>
+
+        <tr>
+            <td>メールアドレス&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="email" id="email" name="email" placeholder="form@example.com" maxlength="100">
+            </td>
+        </tr>
+
+        <tr>
+            <td>利用予定人数&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <input type="text" id="people_num" name="people_num" maxlength="10">人
+                <span>※おおよその人数で構いません</span>
+            </td>
+        </tr>
+
+        <tr>
+            <td>お問い合わせ内容&nbsp;<img src="img/hissu.png"></td>
+            <td>
+                <textarea id="inquiry_detail" name="inquiry_detail" maxlength="2000"></textarea>
+            </td>
+        </tr>
+    </table>
+
+    <div class="form-button">
+        <input type="submit" value="送信">
+    </div>
+
+    <footer>
+        <div class="footer-container">
+            copyright©2024&nbsp;NIPS Co.,Ltd.&nbsp;all&nbsp;rights&nbsp;reserved.
+            <a class="pagetop" href="#">
+                <img src="img/up_arrow.png" alt="トップへ戻る" class="up_arrow">
+            </a>
+        </div>
+    </footer>
+
+</body>
+</html>
